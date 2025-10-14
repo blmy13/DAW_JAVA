d01c129cf8e2122257e160ae6f5c2fe49d619b2a
Àlex Barthelemy <abarthel.daw1r25@lamerce.com>
2025-10-14 20:18:00 +0200

    .

---
+++ src/Bucles/newpackage/Ex2.java
@@ -0,0 +1,22 @@
+package Bucles.newpackage;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+        int max = 0;
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Introdueix un número: ");
+            int num = tec.nextInt();
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("El número més gran és " + max);
+    }
+
+}
