83c9d754a5359a98c9eff52bd18e53d74ddf416f
Alex <abarthel.daw1r25@lamerce.com>
2025-10-18 00:48:30 +0200

    .

--- src/Bucles/Ex15.java
+++ src/Bucles/Ex15.java
@@ -1,21 +1,23 @@
 package Bucles;
 
 import java.util.Scanner;
+
 public class Ex15 {
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
         System.out.println("Introdueix un número");
         int num = tec.nextInt();
-        
-        for (int i = num; i >= 1; i--){
-            for (int j = 1; j <= i; j++){
-                System.out.print(" ");
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print("  ");
             }
-            for (int j = i; j <= num ; j++){
+            for (int k = 1; k <= i; k++){
                 System.out.print("* ");
             }
-            System.out.println("");
+
+            System.out.println();
         }
     }
-    
+
 }
