c3554deb4cdf9d594bd48dfa8560e1fbfc423806
Àlex Barthelemy <abarthel.daw1r25@lamerce.com>
2025-10-21 17:07:39 +0200

    .

--- src/While/Ex13.java
+++ src/While/Ex13.java
@@ -6,26 +6,27 @@ package While;
 import java.util.Scanner;
 
 public class Ex13 {
+
     public static void main(String[] args) {
-        
+
         Scanner tec = new Scanner(System.in);
-        
+
         System.out.println("Introdueix un número. (-1 per acabar)");
         int num = tec.nextInt();
         int numNext;
-        
+
         while (num != -1) {
-            
+
             System.out.println("Introdueix el següent número");
             numNext = tec.nextInt();
-            
-            if (numNext > num){
+
+            if (numNext > num) {
                 System.out.println("El número: " + numNext + " és més gran que: " + num);
             }
-            
-            numNext = num;
-            
+
+            num = numNext;
+
         }
     }
-    
+
 }
