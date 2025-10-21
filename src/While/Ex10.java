7e4615aa0aa01f8b0db544d574c897bfd3fcbe2a
Alex <abarthel.daw1r25@lamerce.com>
2025-10-21 02:13:39 +0200

    .

---
+++ src/While/Ex10.java
@@ -0,0 +1,31 @@
+package While;
+
+//Llegir números i dir si tots són parells.
+
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Introdueix un número.");
+        int num = tec.nextInt();
+
+        boolean parell = false;
+
+        while (num != -1) {
+
+
+            if (num % 2 == 0) {
+                parell = true;
+            }
+
+            System.out.println("Introdueix el següent número.");
+            num = tec.nextInt();
+        }
+
+
+        System.out.println(parell ? "Tots els números que has introduït són parells" : "No tots els números introduïts són parells.");
+    }
+}
