dcc01cad18f6348ea6080f60a34a9f721b69bbdf
Àlex Barthelemy <abarthel.daw1r25@lamerce.com>
2025-10-22 16:41:17 +0200

    .

--- src/While/Extra1.java
+++ src/While/Extra1.java
@@ -6,8 +6,27 @@ exemple : 8217
 package While;
 
 public class Extra1 {
+
     public static void main(String[] args) {
-        
+
+        int num = 1000;
+        int num2;
+        int d1 = 0, d2 = 0, d3 = 0, d4 = 0;
+
+        while (num < 10000) {
+
+            d4 = num % 10;
+            d3 = (num / 10) % 10;
+            d2 = (num / 100) % 10;
+            d1 = num / 1000;
+
+            int sumaParell = d1 + d3;
+            int sumaSenar = d2 + d4;
+
+            if (sumaParell == sumaSenar) {
+                System.out.println("En el número: " + num + " es compleix la condició.");
+            }
+            num++;
+        }
     }
-    
 }
