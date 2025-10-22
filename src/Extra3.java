04e4fb36ea43e3849ce24e06494a9db57a61986b
Àlex Barthelemy <abarthel.daw1r25@lamerce.com>
2025-10-22 17:43:18 +0200

    .

---
+++ src/Extra3.java
@@ -0,0 +1,40 @@
+/*
+Per trobar el màxim comú divisor (mcd) de dos números s’utilitza l’algorisme
+d’Euclides. Donat dos números enters a i b, es mira quin és el més gran. Si el més
+gran és a, es substitueix per a - b; si el més gran és b es substitueix per b – a , i així
+successivament fins que a = b. Aleshores a és el mcd. Escriviu un programa que
+calculi el mcd de dos números.
+ */
+
+import java.util.Scanner;
+
+public class Extra3 {
+    public static void main(String[] args) {
+        
+        Scanner tec = new Scanner(System.in);
+        
+        System.out.println("Introdueix un número.");
+        int numA = tec.nextInt();
+        
+        System.out.println("Introdueix un número.");
+        int numB = tec.nextInt();
+        
+        while (numA != numB) { 
+            
+            if (numA > numB) {
+                
+                numA -= numB;
+                
+            } else {
+                
+                numB -= numA;
+            }
+            
+        }
+        
+        int mcd = numA;
+        
+        System.out.println("El màxim comú divisor és: " + mcd);
+    }
+    
+}
---
+++ src/Extra4.java
@@ -0,0 +1,31 @@
+/*
+Calculeu aproximadament l’arrel quadrada d’un número real positiu r seguint el
+mètode de Newton.
+ */
+
+import java.util.Scanner;
+
+public class Extra4 {
+
+    public static void main(String[] args) {
+
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Introdueix un número.");
+        double numR = tec.nextDouble();
+
+        double xI, xIS;
+
+        xI = numR / 2;
+        xIS = (xI + (numR / xI)) / 2;
+
+        while (Math.abs(xIS - xI) >= 0.0001) {
+            xI = xIS;
+            xIS = (xI + (numR / xI)) / 2;
+
+        }
+        System.out.println("El valor de l'arrel quadrada del número que has introduït és" + xIS);
+
+    }
+
+}
