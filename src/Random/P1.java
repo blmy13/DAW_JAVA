6f58bf7acb563da5758e9c6930e90a95707f8eaf
Alex <abarthel.daw1r25@lamerce.com>
2025-10-28 01:10:23 +0100

    .

--- src/Random/P1.java
+++ src/Random/P1.java
@@ -14,36 +14,38 @@ public class P1 {
         
         //NIVELLS   
         for (int l = 1; l <= nivell; l++) {
+            for (int h = 1; h < nivell; h++) {
 
-            //FIGURA PART SUPERIOR
-            for (int i = 1; i <= mida; i++) {
-                for (int j = 1; j <= mida - i; j++) {
-                    System.out.print(" ");
-                }
-                int ampladaFila = 2 * i - 1;
-                for (int k = 1; k <= ampladaFila; k++) {
-                    if (k == 1 || k == ampladaFila) {
-                        System.out.print("*");
-                    } else {
+                //FIGURA PART SUPERIOR
+                for (int i = 1; i <= mida; i++) {
+                    for (int j = 1; j <= mida - i; j++) {
                         System.out.print(" ");
                     }
+                    int ampladaFila = 2 * i - 1;
+                    for (int k = 1; k <= ampladaFila; k++) {
+                        if (k == 1 || k == ampladaFila) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
                 }
-                System.out.println();
-            }
-            //FIGURA PART INFERIOR
-            for (int i = mida - 1; i >= 1; i--) {
-                for (int j = 1; j <= mida - i; j++) {
-                    System.out.print(" ");
-                }
-                int ampladaFila = 2 * i - 1;
-                for (int k = 1; k <= ampladaFila; k++) {
-                    if (k == 1 || k == ampladaFila) {
-                        System.out.print("*");
-                    } else {
+                //FIGURA PART INFERIOR
+                for (int i = mida - 1; i >= 1; i--) {
+                    for (int j = 1; j <= mida - i; j++) {
                         System.out.print(" ");
                     }
+                    int ampladaFila = 2 * i - 1;
+                    for (int k = 1; k <= ampladaFila; k++) {
+                        if (k == 1 || k == ampladaFila) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
                 }
-                System.out.println();
             }
         }
     }
