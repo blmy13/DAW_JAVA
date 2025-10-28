8c980e3164d4e7b8c2395927ec74bc04ff912aee
Alex <abarthel.daw1r25@lamerce.com>
2025-10-28 22:51:07 +0100

    .

--- src/Random/P1.java
+++ src/Random/P1.java
@@ -7,21 +7,33 @@ public class P1 {
     public static void main(String[] args) {
 
         Scanner tec = new Scanner(System.in);
-        System.out.println("Introdueix la mida del diamant.");
+        System.out.println("Introdueix la mida del diamant:");
         int mida = tec.nextInt();
-        System.out.println("Introdueix el número de nivells");
+        System.out.println("Introdueix el número de nivells (files de la piràmide):");
         int nivell = tec.nextInt();
-        
-        //NIVELLS   
-        for (int l = 1; l <= nivell; l++) {
-            //REPETICIONS HORITZONTALS
-            for (int h = 1; h < nivell; h++) {
 
-                //FIGURA PART SUPERIOR
-                for (int i = 1; i <= mida; i++) {
+        int filesTotal = 2 * mida - 1;
+
+        for (int l = 1; l <= nivell; l++) {
+
+            int diamantsPerFila = (int) Math.pow(2, l - 1);
+
+            for (int filesDiamant = 1; filesDiamant <= filesTotal; filesDiamant++) {
+
+                boolean isTopHalf = filesDiamant <= mida;
+                int i;
+                if (isTopHalf) {
+                    i = filesDiamant;
+                } else {
+                    i = filesTotal - filesDiamant + 1;
+                }
+
+                for (int h = 1; h <= diamantsPerFila; h++) {
+
                     for (int j = 1; j <= mida - i; j++) {
                         System.out.print(" ");
                     }
+
                     int ampladaFila = 2 * i - 1;
                     for (int k = 1; k <= ampladaFila; k++) {
                         if (k == 1 || k == ampladaFila) {
@@ -30,23 +42,13 @@ public class P1 {
                             System.out.print(" ");
                         }
                     }
-                    System.out.println();
-                }
-                //FIGURA PART INFERIOR
-                for (int i = mida - 1; i >= 1; i--) {
-                    for (int j = 1; j <= mida - i; j++) {
+
+                    int espaiRestant = mida - i;
+                    for (int s = 1; s <= espaiRestant; s++) {
                         System.out.print(" ");
                     }
-                    int ampladaFila = 2 * i - 1;
-                    for (int k = 1; k <= ampladaFila; k++) {
-                        if (k == 1 || k == ampladaFila) {
-                            System.out.print("*");
-                        } else {
-                            System.out.print(" ");
-                        }
-                    }
-                    System.out.println();
                 }
+                System.out.println();
             }
         }
     }
