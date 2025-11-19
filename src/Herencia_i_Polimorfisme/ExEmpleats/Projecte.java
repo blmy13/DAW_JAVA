b5e42c022ed8c017825e3abce88072aae6f3e2ad
Àlex Barthelemy <abarthel.daw1r25@lamerce.com>
2025-11-19 20:08:15 +0100

    .

--- src/Herencia_i_Polimorfisme/ExEmpleats/Projecte.java
+++ src/Herencia_i_Polimorfisme/ExEmpleats/Projecte.java
@@ -25,7 +25,7 @@ public class Projecte {
     
     public void mostrarEmpleat() {
         for (Empleat e : empleats) {
-            
+            //mostrar atributs usuaris
         }
         
         
