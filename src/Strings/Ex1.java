3bc0f52599997819e23c3c18fce75974f8fd9d3b
blmy13 <abarthel.daw1r25@lamerce.com>
2025-12-16 20:51:58 +0100

    .

---
+++ src/Strings/Ex1.java
@@ -0,0 +1,25 @@
+/*
+L‟última lletra del DNI pot calcular-se a partir del seus números. Per a això
+només has de dividir el número per 23 i quedar-te amb la resta. Fes un programa
+per calcular la lletra d‟un DNI.
+ */
+package Strings;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    
+    public static void main(String[] args) {
+        
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Introdueix un DNI:");
+        int dni = tec.nextInt();
+        int num = dni % 23;
+        
+        String taula = "TRWAGMYFPDXBNJZSQVHLCKE";
+        System.out.println("La lletra del teu DNI és : " + taula.charAt(num));
+        
+       
+    }
+    
+}
---
+++ src/Strings/Ex2.java
@@ -0,0 +1,20 @@
+/*
+Genera un programa que encripti una cadena de caràcters. Per exemple :
+“avadakedavra” encriptat es “bwbeblfebwsb” i el mètode d‟encriptació és
+canviar un caràcter pel seu successor a l‟abecedari (el successor de „z‟ és „a‟).
+ */
+package Strings;
+
+
+public class Ex2 {
+    
+    public static void main(String[] args) {
+        String missatge = "pastaambpesto";
+        String abecedari = "abcdefghijklmnoprstuvwxyz";
+        
+        for (int i = 0; i < missatge.length(); i++) {
+           
+            
+        }
+    }
+}
