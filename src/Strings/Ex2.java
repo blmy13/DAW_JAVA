292fdcb47c61e776dd486bcc83aeb55cfdd0cf58
Alex <abarthel.daw1r25@lamerce.com>
2025-12-17 14:09:55 +0100

    .

--- src/Strings/Ex2.java
+++ src/Strings/Ex2.java
@@ -7,14 +7,28 @@ package Strings;
 
 
 public class Ex2 {
-    
+
     public static void main(String[] args) {
         String missatge = "pastaambpesto";
         String abecedari = "abcdefghijklmnoprstuvwxyz";
-        
+        String resultat = "";
+
         for (int i = 0; i < missatge.length(); i++) {
-           
-            
+            char lletraActual = missatge.charAt(i);
+
+            int posicio = abecedari.indexOf(lletraActual);
+
+            if (posicio != -1) {
+                int seguentPosicio = (posicio + 1) % abecedari.length();
+                resultat += abecedari.charAt(seguentPosicio);
+            } else {
+                resultat += lletraActual;
+            }
         }
+
+        System.out.println("Missatge original: " + missatge);
+        System.out.println("Missatge encriptat: " + resultat);
+
     }
 }
+
