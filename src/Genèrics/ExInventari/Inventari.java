439c089048fd3867b3cbcf236cbe9228b9a5c815
Alex <abarthel.daw1r25@lamerce.com>
2026-01-14 02:26:16 +0100

    .

--- src/Genèrics/ExInventari/Inventari.java
+++ src/Genèrics/ExInventari/Inventari.java
@@ -10,19 +10,37 @@ public class Inventari<T extends Producte> {
 
         boolean repetit = false;
         int i = 0;
-        while (!repetit && i <= productes.size()) {
+        while (!repetit && i < productes.size()) {
             if (t.getId().equals(productes.get(i).getId())) {
                 repetit = true;
-                if (repetit) {
-                    throw new IllegalArgumentException "Error; estàs intentant afegir un producte repetit"; 
-                }
+                throw new IllegalArgumentException("Error; estàs intentant afegir un producte repetit");
             }
             i++;
-
         }
+        productes.add(t);
     }
 
-    public void consultarProducte(T t) {
+    public Producte consultarProducte(String id) throws IllegalArgumentException {
+        boolean trobat = false;
+        int i = 0;
+        while (!trobat && i < productes.size()) {
+            if (id.equals(productes.get(i).getId())) {
+                trobat = true;
+                return (productes.get(i));
+            }
+            i++;
+        }
+        throw new IllegalArgumentException("Error; el producte no s'ha trobat");
 
+    }
+
+    public void eliminarProducte(String id) throws IllegalArgumentException {
+        consultarProducte(id);
+        productes.remove(id);
+    }
+    public void mostrarTots() {
+        for  (T t : productes) {
+            System.out.println(t);
+        }
     }
 }
