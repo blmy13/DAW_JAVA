7a43973bba8226a566b20cb8961b03f632a6c594
Alex <abarthel.daw1r25@lamerce.com>
2026-02-26 22:38:55 +0100

    .

---
+++ src/Test/Matriu.java
@@ -0,0 +1,17 @@
+package Test;
+//inicialitzar una matriu 2D d'enters i sumar-ne tots els elements recorrent les files i columnes.
+public class Matriu {
+    public static void main(String[] args) {
+
+        int[][] num = {{4, 8, 9}, {8, 7, 4}};
+        int suma = 0;
+
+        for (int[] ints : num) {
+            for (int j = 0; j < num.length; j++) {
+                suma += ints[j];
+            }
+        }
+        System.out.println("Suma dels elements de la matriu: " + suma);
+        
+    }
+}
